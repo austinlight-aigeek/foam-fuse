b1e34bb515b9b0612bb16df8245778a43556704a
Austin Light <austinlight.fl@gmail.com>
2024-03-10 17:36:14 -0400

    Chat and Profile Page completed

---
+++ src/app/main/apps/appsConfigs.js
@@ -0,0 +1,17 @@
+import CalendarAppConfig from './calendar/CalendarAppConfig';
+import ChatAppConfig from './chat/ChatAppConfig';
+import ContactsAppConfig from './contacts/ContactsAppConfig';
+import MailboxAppConfig from './mailbox/MailboxAppConfig';
+import NotesAppConfig from './notes/NotesAppConfig';
+import ProfileAppConfig from './profile/profileAppConfig';
+
+const appsConfigs = [
+  MailboxAppConfig,
+  ContactsAppConfig,
+  CalendarAppConfig,
+  ChatAppConfig,
+  NotesAppConfig,
+  ProfileAppConfig,
+];
+
+export default appsConfigs;
